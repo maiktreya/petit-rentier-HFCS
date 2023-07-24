301505fc4be20f298942342b6e37ebd52ebd610d
MiguelGarciaDuch <miguel.garcia.duch@renesys.es>
2023-07-24 23:26:20 +0200

    first working eff

--- tests/gradient-boost.R
+++ tests/gradient-boost.R
@@ -16,10 +16,10 @@ setnames(
         new = c("class", "worker", "homeowner", "mainres_val")
 )
 dt_eff[renthog < 20000, renthog1 := "a"][renthog > 20000, renthog1 := "b"][renthog > 80000, renthog1 := "c"]
-dt_eff[renthog == "a", renthog1 := 1][renthog1 =="b", renthog := 2][renthog1 =="c", renthog := 3]
+dt_eff[renthog1 == "a", renthog := 1][renthog1 == "b", renthog := 2][renthog1 == "c", renthog := 3]
 
 dt_eff$renthog <- factor(dt_eff$renthog, levels = c(1, 2, 3), labels = c("Low", "Middle", "High"))
-0dt_eff$sex <- factor(dt_eff$sex, levels = c(1, 2), labels = c("Man", "Women"))
+dt_eff$sex <- factor(dt_eff$sex, levels = c(1, 2), labels = c("Man", "Women"))
 dt_eff$bage <- factor(dt_eff$bage, levels = c(1, 2, 3, 4, 5, 6), labels = c("0-34", "35-44", "45-54", "54-65", "65-75", "75"))
 dt_eff$young <- factor(dt_eff$young, levels = c(1, 2), labels = c("Young", "Not-Young"))
 dt_eff$class <- factor(dt_eff$class, levels = c(1, 2, 3, 4, 5, 6), labels = c("worker", "capitalist", "self-employed", "manager", "retired", "inactive"))
