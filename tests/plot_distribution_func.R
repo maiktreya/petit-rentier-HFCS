30a98e79f3924756f1ce8a92486e2e57b3ab72da
MiguelGarciaDuch <miguel.garcia.duch@renesys.es>
2023-08-15 18:26:08 +0200

    standard

--- tests/plot_distribution_func.R
+++ tests/plot_distribution_func.R
@@ -1,6 +1,7 @@
 # Define your weights
 ### WORKSPACE SETUP- MEMORY CLEAN AND PACKAGES IMPORT
-rm(list = ls()) # ENSURE ENVIROMENT IS CLEAN
+rm(list = ls()) # ensure enviroment is clean
+options(scipen = 999) # force to avoid if possible scientific notation in output
 `%>%` <- magrittr::`%>%` # nolint # ALLOW PIPE  MULTI-LOADING WITHOUT MAGRITTR
 c("magrittr", "survey", "dineq", "data.table", "oaxaca", "gtsummary", "xtable", "convey") %>%
         sapply(library, character.only = T)
@@ -42,18 +43,30 @@ dt_effA <- dtlist[[1]][, c("facine3", "renthog", "renthog1", "bage", "homeowner"
 dt_effB <- dtlist[[2]][, c("facine3", "renthog", "renthog1", "bage", "homeowner", "worker", "young", "sex", "class", "actreales", "RIF_actreales")][, identif := 1]
 dt_eff <- rbind(dt_effA, dt_effB)
 sv_eff <- svydesign(ids = ~1, data = as.data.frame(dt_eff), weights = ~ dt_eff$facine3)
-upper_bound <- svyquantile(~actreales, sv_eff, quantiles = c(0.99))[1]$actreales[, "quantile"]
+upper_bound <- svyquantile(~actreales, sv_eff, quantiles = c(0.95))[1]$actreales[, "quantile"]
 
 ######## 3 PLOTTING EMPIRICAL DISTRIBUTIONS
 jpeg(file = "output/rif/img/gini_lorentz.jpeg")
 svylorenz(~actreales, convey_prep(sv_eff), na.rm = T)
 dev.off()
 jpeg(file = "output/rif/img/cdf.jpeg")
-svycdf(~actreales, subset(sv_eff, actreales < upper_bound), na.rm = T) %>% plot()
+cap_s <- svycdf(~actreales, subset(sv_eff, actreales < upper_bound & actreales > 0), na.rm = T)
 dev.off()
 jpeg(file = "output/rif/img/histogram.jpeg")
-svyhist(~actreales, subset(sv_eff, actreales < upper_bound), na.rm = T)
+svyhist(~actreales, subset(sv_eff, actreales < upper_bound & actreales > 0), na.rm = T)
 dev.off()
 jpeg(file = "output/rif/img/emp_histogram.jpeg")
-svysmooth(~actreales, subset(sv_eff, actreales < upper_bound & actreales > 0), na.rm = T) %>% plot()
+cap_s <- svysmooth(~actreales, subset(sv_eff, actreales < upper_bound & worker %in% "Non-Worker" & actreales > 0), na.rm = T)[[1]]
+wor_s <- svysmooth(~actreales, subset(sv_eff, actreales < upper_bound & worker %in% "Worker" & actreales > 0), na.rm = T)[[1]]
+dt <- data.table(cap_s_y = cap_s$y, wor_s_y = wor_s$y, cap_s_x = cap_s$x, wor_s_x = wor_s$x)
+# Find the common x range
+x_range <- range(c(dt$cap_s_x, dt$wor_s_x))
+# Find the common y range
+y_range <- range(c(dt$cap_s_y, dt$wor_s_y))
+# Plot the first empirical distribution with custom axis limits
+plot(dt$cap_s_x, dt$cap_s_y, type = "l", col = "red", xlim = x_range, ylim = y_range, main = "Empirical Distribution Functions", xlab = "Total Wealth", ylab = "Relative Frequency")
+# Add the second empirical distribution
+lines(dt$wor_s_x, dt$wor_s_y, col = "blue")
+# Add a legend
+legend("topright", legend = c("Employers", "Workers"), col = c("red", "blue"), lty = 1)
 dev.off()
