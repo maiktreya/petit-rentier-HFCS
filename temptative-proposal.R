112ada2c26829acb6d2d79e27543e4ead835a512
MiguelGarciaDuch <miguel.garcia.duch@renesys.es>
2023-08-30 23:53:35 +0200

    ewq

--- temptative-proposal.R
+++ temptative-proposal.R
@@ -17,15 +17,13 @@ models_dt <- models_dt_int <- list()
 cpi <- c(73.31, 80.44, 89.11, 93.35, 96.82, 97.98, 100)
 years <- c(2002, 2005, 2008, 2011, 2014, 2017, 2020)
 
-# prepare the RIF
-
 dt_eff$class <- relevel(as.factor(dt_eff$class), ref = "worker")
 dt_eff$bage <- relevel(as.factor(dt_eff$bage), ref = "45-54")
-# Estimate RIF model
 
 for (i in seq_along(years)) {
     dt_transform <- dt_eff[sv_year == years[i]]
     dt_transform[, riquezanet := riquezanet / cpi[i]]
+    # Estimate RIF model
     dt_transform$rif_riquezanet <- rif(dt_transform$riquezanet, method = "quantile", quantile = 0.5, weights = dt_transform$facine3)
     models_dt[[i]] <- lm(rif_riquezanet ~ bage + class, weights = facine3, data = dt_transform)
     models_dt_int[[i]] <- lm(rif_riquezanet ~ bage * class, weights = facine3, data = dt_transform)
