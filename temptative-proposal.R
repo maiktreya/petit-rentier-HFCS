92e99b783aa1e1a52dc5884b1723bdd6085e53a3
MiguelGarciaDuch <miguel.garcia.duch@renesys.es>
2023-08-30 20:14:34 +0200

    LAST

--- temptative-proposal.R
+++ temptative-proposal.R
@@ -11,7 +11,8 @@ dt_eff <- "saves/eff-pool-2002-2020.csv" %>% fread() # Data table con microdatos
 # Convert 'class' and 'bage' to dummy variables
 dt_eff[, ..selected_variables]
 dt_eff[is.na(dt_eff)] <- 0
-final_dt <- final_dt_int <- list()
+final_dt <- final_dt_int <- data.table()
+models_dt <- models_dt_int <- list()
 
 cpi <- c(73.31, 80.44, 89.11, 93.35, 96.82, 97.98, 100)
 years <- c(2002, 2005, 2008, 2011, 2014, 2017, 2020)
@@ -24,36 +25,41 @@ dt_eff$rif_riquezanet <- rif(dt_eff$riquezanet, method = "quantile", quantile = 
 # Estimate RIF model
 
 for (i in seq_along(years)) {
-    final_dt[[i]] <- lm(rif_riquezanet ~ bage + class, data = dt_eff[sv_year == years[i]])
-    final_dt_int[[i]] <- lm(rif_riquezanet ~ bage * class, data = dt_eff[sv_year == years[i]])
+    models_dt[[i]] <- lm(rif_riquezanet ~ bage + class, data = dt_eff[sv_year == years[i]])
+    models_dt_int[[i]] <- lm(rif_riquezanet ~ bage * class, data = dt_eff[sv_year == years[i]])
+    coefs <- coef(summary(lm(rif_riquezanet ~ bage + class, data = dt_eff[sv_year == years[i]])))
+    coefs_int <- coef(summary(lm(rif_riquezanet ~ bage * class, data = dt_eff[sv_year == years[i]])))
+    pre_dt <- c(rbind(coefs[, "Estimate"], coefs[, "Pr(>|t|)"]))
+    pre_dt_int <- c(rbind(coefs_int[, "Estimate"], coefs_int[, "Pr(>|t|)"]))
+    final_dt <- cbind(final_dt, c(years[i], pre_dt))
+    final_dt_int <- cbind(final_dt_int, c(years[i], pre_dt_int))
 }
 
+interleaved_names <- c(rbind(row.names(coefs), rep("coef", 10)))
+interleaved_names_int <- c(rbind(row.names(coefs_int), rep("coef", 28)))
 
-# Assume `final_dt` is your list of lm objects
+final_dt <- cbind(interleaved_names, final_dt)
+final_dt_int <- cbind(interleaved_names_int, final_dt_int)
 
-# Extract the coefficients and p-values from the first model
-coefs <- coef(summary(final_dt[[1]]))
-interleaved <- c(rbind(coefs[, "Estimate"], coefs[, "Pr(>|t|)"]))
-print(interleaved)
-# Combine the coefficients and p-values into a single vector
-
-# Display the final_dt
-
-
-# bst_model02 <- lm(rif_riquezanet ~ bage + class + educ + sex + multipr + direc + as.logical(riquezafin) + inherit, data = dt_eff[sv_year == 2002])
-# bst_model20 <- lm(rif_riquezanet ~ bage + class + educ + sex + multipr + direc + as.logical(riquezafin) + inherit, data = dt_eff[sv_year == 2020])
-# bst_model02int <- lm(rif_riquezanet ~ bage * class + educ + sex + multipr + direc + as.logical(riquezafin) + inherit, data = dt_eff[sv_year == 2002])
-# bst_model20int <- lm(rif_riquezanet ~ bage * class + educ + sex + multipr + direc + as.logical(riquezafin) + inherit, data = dt_eff[sv_year == 2020])
+fwrite(final_dt, file = "final_dt")
+fwrite(final_dt_int, file = "final_dt_int")
 
 sink("output/temptative_multi.txt")
+print("################## MAIN MODEL ###################")
+print(final_dt)
+print("################## INTERACTIONS MODEL ###################")
+print(final_dt_int)
+sink()
+
+sink("output/temptative.txt")
 
 for (i in seq_along(years)) {
     print(paste0("###############", years[i], " ###############"))
-    summary(print(final_dt[[i]]))
+    print(summary(models_dt[[i]]))
 }
 for (i in seq_along(years)) {
     print(paste0("###############", years[i], " INTERACTIONS ###############"))
-    summary(print(final_dt_int[[i]]))
+    print(summary(models_dt_int[[i]]))
 }
 
 sink()
