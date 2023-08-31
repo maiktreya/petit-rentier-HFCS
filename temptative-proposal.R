4ba8e0272f607dc707e3a8c5e6e8a410ebd8c963
MiguelGarciaDuch <miguel.garcia.duch@renesys.es>
2023-08-31 02:32:58 +0200

    ads

--- temptative-proposal.R
+++ temptative-proposal.R
@@ -1,8 +1,7 @@
 ### WORKSPACE SETUP- MEMORY CLEAN AND PACKAGES IMPORT
 `%>%` <- magrittr::`%>%` # nolint
 options(scipen = 99)
-rif_var <- "quantile"
-transform <- F
+rif_var <- "gini"
 c("survey", "data.table", "dineq", "xgboost") %>% sapply(library, character.only = T)
 selected_variables <- c(
     "facine3", "renthog", "renthog1", "bage", "homeowner", "worker", "young", "sex", "class",
@@ -16,7 +15,7 @@ dt_eff[is.na(dt_eff)] <- 0
 final_dt <- final_dt_int <- data.table()
 models_dt <- models_dt_int <- list()
 
-cpi <- c(73.31, 80.44, 89.11, 93.35, 96.82, 97.98, 100)
+cpi <- c(73.31, 80.44, 89.11, 93.35, 96.82, 97.98, 100) / 100
 years <- c(2002, 2005, 2008, 2011, 2014, 2017, 2020)
 
 dt_eff$class <- relevel(as.factor(dt_eff$class), ref = "self-employed")
@@ -24,24 +23,28 @@ dt_eff$bage <- relevel(as.factor(dt_eff$bage), ref = "45-54")
 
 for (i in seq_along(years)) {
     dt_transform <- dt_eff[sv_year == years[i]]
-    if (transform = T) dt_transform[, riquezanet := riquezanet / (cpi[i] / 100)]
     # Estimate RIF model
     dt_transform$rif_riquezanet <- rif(dt_transform$riquezanet, method = as.character(rif_var), quantile = 0.5, weights = dt_transform$facine3)
     models_dt[[i]] <- lm(rif_riquezanet ~ bage + class, weights = facine3, data = dt_transform)
     models_dt_int[[i]] <- lm(rif_riquezanet ~ bage * class, weights = facine3, data = dt_transform)
-    coefs <- coef(summary(lm(rif_riquezanet ~ bage + class, weights = facine3, data = dt_transform)))
-    coefs_int <- coef(summary(lm(rif_riquezanet ~ bage * class, weights = facine3, data = dt_transform)))
+    coefs <- coef(summary(lm(rif_riquezanet ~ bage + class, weights = facine3, data = dt_transform))) %>% data.table()
+    coefs_int <- coef(summary(lm(rif_riquezanet ~ bage * class, weights = facine3, data = dt_transform))) %>% data.table()
+    coefs[, Estimate := Estimate / cpi[i]]
+    coefs_int[, Estimate := Estimate / cpi[i]]
+    coefs <- as.data.frame(coefs)
+    coefs_int <- as.data.frame(coefs)
     pre_dt <- c(rbind(coefs[, "Estimate"], coefs[, "Pr(>|t|)"]))
     pre_dt_int <- c(rbind(coefs_int[, "Estimate"], coefs_int[, "Pr(>|t|)"]))
     final_dt <- cbind(final_dt, c(years[i], pre_dt))
     final_dt_int <- cbind(final_dt_int, c(years[i], pre_dt_int))
 }
 
-interleaved_names <- c("year", rbind(row.names(coefs), rep("p-val", 10)))
-interleaved_names_int <- c("year", rbind(row.names(coefs_int), rep("p-val", 28)))
+interleaved_names <- c(rbind(row.names(coefs), rep("p-val", 10)))
+interleaved_names_int <- c(rbind(row.names(coefs_int), rep("p-val", 28)))
 
 final_dt <- cbind(interleaved_names, round(final_dt, 3))
 final_dt_int <- cbind(interleaved_names_int, round(final_dt_int, 3))
+colnames(final_dt) <- colnames(final_dt_int) <- c("vars", years)
 
 fwrite(final_dt, file = paste0("output/", rif_var, "_final_dt.csv"))
 fwrite(final_dt_int, file = paste0("output/", rif_var, "_final_dt_int.csv"))
