acec868a715b6f373f6e8d62382d12fb4f9a1121
MiguelGarciaDuch <miguel.garcia.duch@renesys.es>
2023-09-13 04:38:45 +0200

    last

--- logit-ecv.R
+++ logit-ecv.R
@@ -33,7 +33,7 @@ options(scipen = 9999)
 ### PARAMETERS AND VARIABLES TO INITIALIZE
 quantile_cuts <- c(.25, .5, .75, .9, .99, .999)
 important_variables <- c("PB040", "HY040N", "DB040", "HY090G", "HY100N", "HY130N", "HH021", "PY035N", "PB140", "PL031", "PL040", "PL051", "PY010N", "PB110")
-years <- c(2004, 2022)
+years <- c(2021, 2022)
 final_props <- join_ecv <- data.table()
 
 for (i in seq_along(years)) {
@@ -59,10 +59,30 @@ for (i in seq_along(years)) {
     survey_ecv[AGE == 0, AGE := NA]
     survey_ecv[PL040C == 0, PL040C := NA]
 
+
+
+
+
+
+
+
     setnames(survey_ecv,
-        old = c("HH021", "PL040C", "AGE", "PB020", "PB140", "HX040", "HY040N", "HY090N"),
-        new = c("tenancy", "class", "bage", "country", "birth", "members", "hous-rent", "profit")
+        old = c("RB090", "HH021", "PL040C", "AGE", "RB290", "PB140", "HX040", "HY040N", "HY090N"),
+        new = c("sex", "tenancy", "class", "bage", "country", "birth", "members", "housrent", "profit")
     )
+
+
+
+    survey_ecv$class <- factor(survey_ecv$class, levels = c(1, 2, 3, 4), labels = c("employer", "self-employed", "worker", "manager"))
+    survey_ecv$bage <- factor(survey_ecv$bage, levels = c(1, 2, 3, 4, 5, 6), labels = c("0-34", "35-44", "45-54", "54-65", "65-75", "75"))
+    survey_ecv$sex <- factor(survey_ecv$sex, levels = c(1, 2), labels = c("Man", "Women"))
+    survey_ecv$housrent <- factor(as.integer(survey_ecv$housrent > 0), levels = c(0, 1), labels = c("Norent", "Yesrent"))
+    survey_ecv$profit <- factor(as.integer(survey_ecv$profit > 0), levels = c(0, 1), labels = c("Noprofit", "Yesprofit"))
+    survey_ecv$country <- factor(survey_ecv$country)
+
+
+
+
 
     join_ecv <- rbind(join_ecv, survey_ecv, fill = T)
 
@@ -72,7 +92,7 @@ for (i in seq_along(years)) {
         weights = ~ survey_ecv$PB040
     ))
 
-    model <- svyglm(rentsbi ~ factor(class) + factor(bage) + factor(tenancy), design = survey_total)
+    model <- svyglm(rentsbi ~ class + bage + housrent + profit + sex, design = survey_total)
 
     props <- svyby(~rentsbi, ~class, svymean, design = survey_total, na.rm = T)
 
@@ -90,6 +110,11 @@ survey_join <- as.svydesign2(svydesign(
 # print(final_props)
 reg <- svyglm(outcome ~ treatment * period, design = survey_join, family = "quasibinomial")
 
+
+
+
+sink(paste0("LOGIT.csv"))
 model %>%
     summary() %>%
     print()
+sink()
