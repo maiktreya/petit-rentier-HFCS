6a125d815f7f0904152c7923e8232afe8ad7f3e1
il    iciuv <miguel.garcia.duch@gmail.com>
2023-10-17 22:37:06 +0200

    last

--- main.R
+++ main.R
@@ -1,16 +1,16 @@
 ### WORKSPACE SETUP- MEMORY CLEAN AND PACKAGES IMPORT
 `%>%` <- magrittr::`%>%` # nolint
-options(scipen = 99)
-rif_var <- "quantile"
 c("survey", "data.table", "dineq", "xgboost") %>% sapply(library, character.only = T)
-selected_variables <- c(
-    "facine3", "renthog", "renthog1", "bage", "homeowner", "worker", "young", "sex", "class",
-    "actreales", "riquezanet", "riquezafin", "educ", "auton", "class",
-    "tipo_auton", "direc", "multipr", "useprop", "inherit"
-)
-dt_eff <- "saves/eff-pool-2002-2020.csv" %>% fread() # Data table con microdatos anuales
-# Convert 'class' and 'bage' to dummy variables
-dt_eff[, ..selected_variables]
-dt_eff[is.na(dt_eff)] <- 0
+options(scipen = 99)
+
 final_dt <- data.table()
 models_dt <- list()
+years <- c(2002, 2005, 2008, 2011, 2014, 2017, 2020)
+rif_var <- "quantile"
+dt_eff <- "saves/eff-pool-2002-2020.csv" %>% fread() # Data table con microdatos anuales
+dt_eff[, rentsbi := 0][rents >= 1000, rentsbi := 1]
+
+
+for (i in years) {
+    final_dt <- dt_eff[sv_year == i]
+}
