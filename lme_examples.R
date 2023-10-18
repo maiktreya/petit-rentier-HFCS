d963e1a2bc1d9c805eacfe0a444e2930a3a60b33
il    iciuv <miguel.garcia.duch@gmail.com>
2023-10-18 12:37:23 +0200

    last

--- lme_examples.R
+++ lme_examples.R
@@ -2,22 +2,28 @@
 library(magrittr)
 library(data.table)
 library(lme4)
-## linear mixed models - reference values from older code
-ndays <- length(unique(sleepstudy$Days))
-nsubjects <- length(sleepstudy$Days) / ndays
-sleepstudy <- data.table(sleepstudy)
 
+# import test pseudo-panel statistics
 dt_eff <- fread("output/rentsbi.csv")
+
 dt_eff[, variable_num := 0][variable == "prop_ren_w", variable_num := 1][variable == "prop_ren_k", variable_num := 2][, years_bi := rep(1:7, 2)]
 dt_mixed <- dt_eff[, list(years_bi, variable_num, value)]
 setnames(dt_mixed,
     old = c("years_bi", "variable_num", "value"),
-    new = c("Days", "Subject", "Reaction")
+    new = c("year", "class", "rents")
 )
-print(dt_mixed)
-# lmer(Reaction ~ Days + (Days | Subject), data = sleepstudy)
-# lmer(rentsbi ~ year + (year | class), data = dt_eff)
-fm1 <- lmer(Reaction ~ Days + (Days | Subject), data = dt_mixed)
+dt_mixed$Covariate1 <- rbinom(n = nrow(dt_mixed), size = 10, prob = 0.2)
+
+## estimate random correlated effects (Mixed Models)
+
+# only effects, no covariates
+fm1 <- lmer(rents ~ year + (year | class), data = dt_mixed)
 fm1 %>%
+    summary() %>%
+    print()
+
+# effects + one covariate/regressor
+fm2 <- lmer(rents ~ year + Covariate1 + (year | class), data = dt_mixed)
+fm2 %>%
     summary() %>%
     print()
