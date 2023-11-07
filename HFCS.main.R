0e32778e8c0b4ee235eebc65f8470ac5d8dddf7f
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-07 21:41:23 +0100

    last

--- HFCS.main.R
+++ HFCS.main.R
@@ -1,26 +1,31 @@
 library(magrittr)
 library(data.table)
-rm(list = ls())
-
-test2_1 <- fread(".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/D1.csv")
-test2_2 <- fread(".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/D2.csv")
-test2_3 <- fread(".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/D3.csv")
-test2_4 <- fread(".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/D4.csv")
-test2_5 <- fread(".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/D5.csv")
-test2 <- rbind(test2_1, test2_2, test2_3, test2_4, test2_5)
-test2 %>%
-    names() %>%
-    print()
 
 path_string <- ".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/"
-final_dt <- data.table()
+final_dt_h <- final_dt_p <- data.table()
 codes <- c("H", "HN", "D", "P", "PN")
 
-for (i in codes) {
+# JOINT HOUSEHOLD MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
+for (i in codes[1:3]) {
     imp_dt <- data.table()
     for (j in 1:5) {
         imp <- fread(paste0(path_string, i, j, ".csv"))
         imp_dt <- rbind(imp_dt, imp)
     }
-    final_dt <- cbind(final_dt, imp_dt)
+    final_dt_h <- cbind(final_dt_h, imp_dt)
+    rm(list = setdiff(ls(), c("final_dt_h", "final_dt_p", "path_string", "codes")))
 }
+
+# JOINT PERSONAL MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
+for (i in codes[4:5]) {
+    imp_dt <- data.table()
+    for (j in 1:5) {
+        imp <- fread(paste0(path_string, i, j, ".csv"))
+        imp_dt <- rbind(imp_dt, imp)
+    }
+    final_dt_p <- cbind(final_dt_p, imp_dt)
+    rm(list = setdiff(ls(), c("final_dt_h", "final_dt_p", "path_string", "codes")))
+}
+
+# MERGE
+final_dt <- melt(final_dt_h, final_dt_p)
