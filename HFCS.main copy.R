985454ec6944b7318e061feefa41309d8f971fae
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-08 21:29:07 +0100

    last

--- HFCS.main copy.R
+++ HFCS.main copy.R
@@ -43,7 +43,7 @@ final_dt_h <- final_dt_h[, !duplicated(names(final_dt_h)), with = FALSE][, !nm1,
 final_dt <- merge(final_dt_h, final_dt_p, by = "ID", all.x = TRUE)
 
 hfcs.design <- svydesign(
-    ids = ~SA0010,
+    ids = ~SA0010.x,
     weights = ~HW0010,
     data = data.frame(final_dt),
 )
@@ -58,3 +58,18 @@ final_dt_reduced %>% fwrite(".datasets/HFCS/totals/total2011.csv.gz", compress =
 # HID -> household id A (personal file)
 # SA0010 -> household id B
 # SA0100 -> country
+# RA0400 -> country birth
+# RA0200 -> gender
+# RAO300 -> age
+# PA0100 -> martial status
+# PA0200 -> education
+# HB0100 -> household size
+# PE0100x -> labour status
+# PE0200 -> status in employment -> (nº of workers: 1, employee 2, Self-employed 3, with employee)
+# PE0300 -> job description (ISCO)
+# PE0300 -> main employment (NACE)
+###########################################
+# HD0210 -> nº of businesses
+# PG0100 -> employee income
+# HG0200 -> income from private transfers (group 7 income)
+# DI2000 -> total household income (aggregated in D category)
