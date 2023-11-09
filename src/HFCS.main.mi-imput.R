aa56c9d7267487b178855472d6900e41fa374409
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-09 17:07:32 +0100

    LAST

--- src/HFCS.main.mi-imput.R
+++ src/HFCS.main.mi-imput.R
@@ -7,15 +7,37 @@ library(mitools)
 # Clean and define hardcoded global variables
 rm(list = ls())
 path_string <- ".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/"
-final_dt_h <- final_dt_p <- tab <- imp <- list()
+final_dt_h <- final_dt_p <- imp_design <- imp <- list()
 codes <- c("H", "HN", "D", "P", "PN")
 
 # JOINT HOUSEHOLD MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
-for (j in 1:5) final_dt_h[[j]] <- fread(paste0(path_string, "H", j, ".csv"))[, imp := j]
+for (j in 1:5) final_dt_h[[j]] <- fread(paste0(path_string, "H", j, ".csv"))[, imp := j] %>% data.frame()
 # JOINT PERSONAL MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
-for (k in 1:5) final_dt_p[[k]] <- fread(paste0(path_string, "P", k, ".csv"))[, imp := k]
+for (k in 1:5) final_dt_p[[k]] <- fread(paste0(path_string, "P", k, ".csv"))[, imp := k] %>% data.frame()
 
 for (i in 1:5) {
-    tab[[i]] <- final_dt_h[[i]][, c("SA0010", "SA0100", "HW0010", "IM0100", "HB0100")]
+    tab <- final_dt_h[[i]][, c("SA0010", "SA0100", "HW0010", "IM0100", "HB0100")]
     imp[[i]] <- merge(tab, final_dt_p[[i]], by = c("SA0010", "SA0100", "IM0100"))
 }
+
+######## SURVEY MANAGEMENT
+W <- fread(".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/W.csv") %>% data.frame()
+repweg <- dplyr::select(W, "wr0001":"wr1000")
+
+for (n in 1:5) {
+    imp_design[[n]] <- svydesign(
+        ids = ~SA0010,
+        weights = ~HW0010,
+        data = imp[[n]],
+    )
+}
+hfcs <- svrepdesign(
+    repweights = repweg,
+    weights = ~HW0010,
+    data = imputationList(imp_design),
+    scale = 1,
+    rscale = rep(1 / 999, 1000),
+    mse = FALSE, type = "other",
+    combined.weights = TRUE,
+    na.rm = TRUE
+)
