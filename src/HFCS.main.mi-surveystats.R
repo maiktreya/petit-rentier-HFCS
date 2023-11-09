34f4ff5ca01989afe0214e814597924389ea1dcc
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-09 21:03:57 +0100

    last

--- src/HFCS.main.mi-surveystats.R
+++ src/HFCS.main.mi-surveystats.R
@@ -7,7 +7,6 @@ library(mitools)
 # Clean and define hardcoded global variables
 rm(list = ls())
 path_string <- ".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/"
-final_dt_h <- final_dt_p <- imp_design <- imp <- list()
 codes <- c("H", "HN", "D", "P", "PN")
 
 # Import and measure performance of survey with multiple imputations
