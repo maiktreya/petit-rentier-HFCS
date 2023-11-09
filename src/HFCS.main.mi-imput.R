b7dd015de89c1571762f011c4f8ded99422a5ef0
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-09 19:43:01 +0100

    last

--- src/HFCS.main.mi-imput.R
+++ src/HFCS.main.mi-imput.R
@@ -11,14 +11,12 @@ path_string <- ".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/"
 final_dt_h <- final_dt_p <- imp_design <- imp <- list()
 codes <- c("H", "HN", "D", "P", "PN")
 
-# JOINT HOUSEHOLD MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
-for (j in 1:5) final_dt_h[[j]] <- fread(paste0(path_string, "H", j, ".csv"))[, imp := j] %>% data.frame()
-# JOINT PERSONAL MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
-for (k in 1:5) final_dt_p[[k]] <- fread(paste0(path_string, "P", k, ".csv"))[, imp := k] %>% data.frame()
-
+# JOINT MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
+for (j in 1:5) final_dt_h[[j]] <- fread(paste0(path_string, "H", j, ".csv"))[, imp := j] # household
+for (k in 1:5) final_dt_p[[k]] <- fread(paste0(path_string, "P", k, ".csv"))[, imp := k] # personal
 for (i in 1:5) {
     tab <- final_dt_h[[i]][, c("SA0010", "SA0100", "HW0010", "IM0100", "HB0100")]
-    imp[[i]] <- merge(tab, final_dt_p[[i]], by = c("SA0010", "SA0100", "IM0100"))
+    imp[[i]] <- merge(tab, final_dt_p[[i]], by = c("SA0010", "SA0100", "IM0100")) %>% data.frame()
 }
 
 ######## SURVEY MANAGEMENT
