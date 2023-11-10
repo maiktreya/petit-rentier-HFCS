f85a6ee5abb1cb91fe176e8eb38561f7328a91f7
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-10 18:32:09 +0100

    last

--- src/svyrepdesign/HFCS.main.mi-input.R
+++ src/svyrepdesign/HFCS.main.mi-input.R
@@ -7,23 +7,20 @@ library(mitools)
 # Clean and define hardcoded global variables
 rm(list = ls())
 init_time <- Sys.time()
-path_string <- ".datasets/HFCS/csv/HFCS_UDB_4_0_ASCII/"
-final_dt_h <- list()
-codes <- c("H", "HN", "D", "P", "PN")
+path_string <- ".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/"
+imp <- list()
 
 # JOINT MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
-for (j in 1:5) final_dt_h[[j]] <- data.frame(fread(paste0(path_string, "h", j, ".csv")))
-
-
+for (j in 1:5) imp[[j]] <- fread(paste0(path_string, "H", j, ".csv")) %>% data.frame()
 
 ######## SURVEY MANAGEMENT
-W <- fread(paste0(path_string, "w.csv")) %>% data.frame()
-repweg <- dplyr::select(W, "wr0001":"wr1000") %>% na.omit(method = "mean")
+W <- fread(paste0(path_string, "W-fixed.csv")) %>% data.frame()
+repweg <- dplyr::select(W, "wr0001":"wr1000")
 
 hfcs <- svrepdesign(
     repweights = repweg,
-    weights = ~hw0010,
-    data = imputationList(final_dt_h),
+    weights = ~HW0010,
+    data = imputationList(imp),
     scale = 1,
     rscale = rep(1 / 999, 1000),
     mse = FALSE, type = "other",
