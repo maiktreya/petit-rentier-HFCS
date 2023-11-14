62f9b2c01b9bab89e028029b4fa11c8d9787d931
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-14 04:50:42 +0100

    last

--- src/svydesign/HFCS.main-grouped.R
+++ src/svydesign/HFCS.main-grouped.R
@@ -6,23 +6,54 @@ library(mitools)
 
 # Clean and define hardcoded global variables
 rm(list = ls())
-path_string <- ".datasets/HFCS/csv/HFCS_UDB_4_0_ASCII/"
+path_string <- ".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/"
 final_dt_h <- final_dt_p <- designs <- imp <- list()
-codes <- c("H", "HN", "D", "P", "PN")
+country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
+n <- country_code[1]
+# JOINT MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
+imp <- impH <- impD <- list()
 
 # JOINT MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
-for (j in 1:5) final_dt_h[[j]] <- fread(paste0(path_string, "h", j, ".csv"))[, imp := j] # household
-for (k in 1:5) final_dt_p[[k]] <- fread(paste0(path_string, "p", k, ".csv"))[, imp := k] # personal
+for (j in 1:5) imp[[j]] <- fread(paste0(path_string, "P", j, ".csv"))[SA0100 == n]
+for (k in 1:5) impD[[k]] <- fread(paste0(path_string, "D", k, ".csv"))[SA0100 == n]
+for (h in 1:5) impH[[h]] <- fread(paste0(path_string, "H", h, ".csv"))[SA0100 == n]
+for (i in 1:5) imp[[i]] <- merge(imp[[i]], impH[[i]], by = c("SA0010", "SA0100", "IM0100"))
+for (j in 1:5) imp[[j]] <- merge(imp[[j]], impD[[j]], by = c("SA0010", "SA0100", "IM0100"))
 for (i in 1:5) {
-    tab <- final_dt_h[[i]][, c("SA0010", "SA0100", "HW0010", "IM0100", "HB0100")]
-    imp[[i]] <- merge(tab, final_dt_p[[i]], by = c("SA0010", "SA0100", "IM0100")) %>% data.frame()
+    transf <- imp[[i]]
+    setnames(transf,
+        c(
+            "DHAGEH1", "DH0001", "DHEDUH1", "DHGENDERH1", "DHEMPH1", "DHHST",
+            "DI1300", "DI1400", "DI1520", "DI1700", "DI2000",
+            "DN3001", "DA2100", "DA1120", "DA1110", "DA1400", "DA1200", "DA1000",
+            "HD0210", "HB2900", "HB2410", "PE0200", "PE0300", "PE0400"
+        ),
+        new = c(
+            "age_ref", "hsize", "edu_ref", "head_gendr", "employm", "tenan",
+            "rental", "financ", "pvpens", "pvtran", "income",
+            "net_we", "net_fi", "other", "main", "real", "bussiness", "total_real",
+            "num_bs", "val_op", "num_op", "status", "d_isco", "d_nace"
+        )
+    )
+    transf <- transf[
+        RA0010 == DHIDH1,
+        c(
+            "age_ref", "hsize", "edu_ref", "head_gendr", "employm", "tenan",
+            "rental", "financ", "pvpens", "pvtran", "income",
+            "net_we", "net_fi", "other", "main", "real", "bussiness", "total_real",
+            "num_bs", "val_op", "num_op", "status", "d_isco", "d_nace",
+            "SA0100", "HW0010.x"
+        )
+    ]
+    transf[, rentsbi := 0][income > 0 & (financ / income) > 0.1, rentsbi := 1]
+    imp[[i]] <- transf
 }
 # Loop through each set of imputations and create svydesign objects
 for (i in 1:5) {
     # Create the svydesign object for the i-th imputation
     designs[[i]] <- svydesign(
-        ids = ~SA0010,
-        weights = ~HW0010,
+        ids = ~1,
+        weights = ~HW0010.x,
         data = imp[[i]],
         strata = ~SA0100,
         nest = TRUE
@@ -33,7 +64,7 @@ for (i in 1:5) {
 means <- numeric(length(designs))
 
 # Loop through each svydesign object and calculate the mean of HB0100
-for (i in 1:5) means[i] <- svymean(~HB0100, designs[[i]], na.rm = TRUE)
+for (i in 1:5) means[i] <- svymean(~rentsbi, designs[[i]], na.rm = TRUE)
 
 
 # Calculate the average mean across all imputations
