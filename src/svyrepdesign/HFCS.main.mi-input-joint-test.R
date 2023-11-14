69870df42d510158776e9f57bc8cc9040f50ec0c
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-14 05:13:18 +0100

    last

--- src/svyrepdesign/HFCS.main.mi-input-joint-test.R
+++ src/svyrepdesign/HFCS.main.mi-input-joint-test.R
@@ -7,26 +7,28 @@ library(mitools)
 # Clean and define hardcoded global variables
 rm(list = ls())
 init_time <- Sys.time()
-path_string <- ".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/"
+path_stringA <- ".datasets/HFCS/csv/"
+path_stringB <- "HFCS_UDB_2_5_ASCII"
+path_string <- paste0(path_stringA, path_stringB, "/")
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
 
 for (n in country_code) {
     imp <- impH <- impD <- list()
 
-    # JOINT MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
-    for (j in 1:5) imp[[j]] <- fread(paste0(path_string, "P", j, ".csv"))[SA0100 == n]
-    for (k in 1:5) impD[[k]] <- fread(paste0(path_string, "D", k, ".csv"))[SA0100 == n]
-    for (h in 1:5) impH[[h]] <- fread(paste0(path_string, "H", h, ".csv"))[SA0100 == n]
-    for (i in 1:5) imp[[i]] <- merge(imp[[i]], impH[[i]], by = c("SA0010", "SA0100", "IM0100"))
-    for (j in 1:5) imp[[j]] <- merge(imp[[j]], impD[[j]], by = c("SA0010", "SA0100", "IM0100"))
+    # joint matrix pre summing imputations (year-wave)
+    for (j in 1:5) imp[[j]] <- fread(paste0(path_string, "p", j, ".csv"))[sa0100 == n]
+    for (k in 1:5) impD[[k]] <- fread(paste0(path_string, "d", k, ".csv"))[sa0100 == n]
+    for (h in 1:5) impH[[h]] <- fread(paste0(path_string, "h", h, ".csv"))[sa0100 == n]
+    for (i in 1:5) imp[[i]] <- merge(imp[[i]], impH[[i]], by = c("sa0010", "sa0100", "im0100"))
+    for (j in 1:5) imp[[j]] <- merge(imp[[j]], impD[[j]], by = c("sa0010", "sa0100", "im0100"))
     for (i in 1:5) {
         transf <- imp[[i]]
         setnames(transf,
             c(
-                "DHAGEH1", "DH0001", "DHEDUH1", "DHGENDERH1", "DHEMPH1", "DHHST",
-                "DI1300", "DI1400", "DI1520", "DI1700", "DI2000",
-                "DN3001", "DA2100", "DA1120", "DA1110", "DA1400", "DA1200", "DA1000",
-                "HD0210", "HB2900", "HB2410", "PE0200", "PE0300", "PE0400"
+                "dhageh1", "dh0001", "dheduh1", "dhgenderh1", "dhemph1", "dhhst",
+                "di1300", "di1400", "di1520", "di1700", "di2000",
+                "dn3001", "da2100", "da1120", "da1110", "da1400", "da1200", "da1000",
+                "hd0210", "hb2900", "hb2410", "pe0200", "pe0300", "pe0400"
             ),
             new = c(
                 "age_ref", "hsize", "edu_ref", "head_gendr", "employm", "tenan",
@@ -36,25 +38,25 @@ for (n in country_code) {
             )
         )
         transf <- transf[
-            RA0010 == DHIDH1,
+            ra0010 == dhidh1,
             c(
                 "age_ref", "hsize", "edu_ref", "head_gendr", "employm", "tenan",
                 "rental", "financ", "pvpens", "pvtran", "income",
                 "net_we", "net_fi", "other", "main", "real", "bussiness", "total_real",
                 "num_bs", "val_op", "num_op", "status", "d_isco", "d_nace",
-                "SA0100", "HW0010.x"
+                "sa0100", "hw0010.x"
             )
         ]
-        transf[, rentsbi := 0][income > 0 & (financ / income) > 0.1, rentsbi := 1]
+        transf[, rentsbi := 0][income > 0 & (as.numeric(financ) / as.numeric(income)) > 0.1, rentsbi := 1]
         imp[[i]] <- transf
     }
 
-    ######## SURVEY MANAGEMENT
-    W <- fread(paste0(path_string, "W-fixed.csv"))[SA0100 == n] %>% data.frame()
-    repweg <- dplyr::select(W, "wr0001":"wr1000")
+    ######## survey management
+    w <- fread(paste0(path_string, "W-fixed.csv"))[sa0100 == n] %>% data.frame()
+    repweg <- dplyr::select(w, "wr0001":"wr1000")
     hfcs <- svrepdesign(
         repweights = repweg,
-        weights = ~HW0010.x,
+        weights = ~hw0010.x,
         data = imputationList(imp),
         scale = 1,
         rscale = rep(1 / 999, 1000),
@@ -63,7 +65,7 @@ for (n in country_code) {
         combined.weights = TRUE
     )
     (Sys.time() - init_time) %>% print()
-    saveRDS(hfcs, file = paste0("saves/", n, "hfcs.RDS"))
+    saveRDS(hfcs, file = paste0("saves/", path_stringB, "/", n, "hfcs.RDS"))
     (Sys.time() - init_time) %>% print()
     rm(list = setdiff(ls(), c("init_time", "country_code", "path_string")))
 }
--- src/weights-fix/weights.R
+++ src/weights-fix/weights.R
@@ -3,15 +3,15 @@ library(data.table) # only dependency needed. Optimized for large datasets
 
 ######## SURVEY MANAGEMENT
 rm(list = ls()) # clean enviroment before proceeding
-path_string <- ".datasets/HFCS/csv/HFCS_UDB_1_5_ASCII/" # I/= csv folder
+path_string <- ".datasets/HFCS/csv/HFCS_UDB_4_0_ASCII/" # I/O csv folder
 imp <- list() # object for imputated datasets with sample weights
 
 # JOINT MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
-for (j in 1:5) imp[[j]] <- fread(paste0(path_string, "H", j, ".csv"))
-W <- fread(paste0(path_string, "W.csv"))
+for (j in 1:5) imp[[j]] <- fread(paste0(path_string, "h", j, ".csv"))
+W <- fread(paste0(path_string, "w.csv"))
 
 # Merge weights with first imputation data and order
-W <- merge(W, imp[[1]][, .(ID, HW0010)], by = "ID")[order(SA0100, SA0010)]
+W <- merge(W, imp[[1]][, .(id, hw0010)], by = "id")[order(sa0100, sa0010)]
 
 # Calculate missing data proportions and replace missing values
 W[, (paste0("miss_", names(W)[4:ncol(W)])) := lapply(.SD, function(x) mean(is.na(x))), .SDcols = 4:ncol(W)]
@@ -22,7 +22,7 @@ wr_cols <- names(W)[grep("^wr", names(W))]
 if (length(wr_cols) > 0) {
     for (wr_col in wr_cols) {
         miss_col <- paste0("miss_", wr_col)
-        W[get(miss_col) == 1, (wr_col) := fifelse(is.na(get(wr_col)), HW0010, get(wr_col))]
+        W[get(miss_col) == 1, (wr_col) := fifelse(is.na(get(wr_col)), hw0010, get(wr_col))]
     }
 
     # Ensure no NA values in weights
