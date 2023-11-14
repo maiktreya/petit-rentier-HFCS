847a03b725174d5d8dc7a12f49946d8de51a8d25
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-14 19:23:55 +0100

    last

--- src/svymodels/HFCS.main.mi-survey-stats.R
+++ src/svymodels/HFCS.main.mi-survey-stats.R
@@ -1,4 +1,5 @@
 # HFCS MAIN FILE FOR IMPORTING AND MERGING IMPUTATIONS FOR DISTINCT HFCS WAVES
+
 library(magrittr)
 library(data.table)
 library(survey)
@@ -10,18 +11,24 @@ path_stringA <- ".datasets/HFCS/csv/HFCS_UDB_"
 path_stringB <- c("1_5", "2_5", "3_3", "4_0")
 path_year <- c(2011, 2013, 2017, 2020)
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-country_mean <- list()
+country_mean <- c()
+year_mean <- data.table()
 
-for (i in 1:1) {
+for (i in seq_along(path_stringB)) {
     path_string <- paste0(path_stringA, path_stringB[i], "_ASCII/")
     for (selected in country_code) {
         # Import and measure performance of survey with multiple imputations
         hfcs <- readRDS(paste0("saves/HFCS_UDB_", path_stringB[i], "_ASCII/", selected, "hfcs.RDS"))
 
         # Combine the mean results from all imputed datasets using Rubin's rules
-        pre <- with(hfcs, svyglm(rentsbi ~ income + head_gendr + edu_ref + hsize, family = quasibinomial())) %>% MIcombine()
+        pre <- with(hfcs, svymean(~rentsbi)) %>% MIcombine()
         country_mean[[selected]] <- pre$coefficients
     }
     # Print the combined mean estimate and its associated standard error
-    country_mean %>% print()
+    country_mean <- country_mean %>% as.data.frame()
+    year_mean <- rbind(year_mean, country_mean)
 }
+year_mean <- year_mean %>% t()
+colnames(year_mean) <- path_year
+year_mean %>% print()
+year_mean %>% fwrite("saves/rentsbi.csv")
