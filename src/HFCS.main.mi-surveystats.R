402e7e2a666842a5f2bc1a9c9fab37200c1d80d9
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-14 05:44:43 +0100

    last

--- src/HFCS.main.mi-surveystats.R
+++ src/HFCS.main.mi-surveystats.R
@@ -6,21 +6,27 @@ library(mitools)
 
 # Clean and define hardcoded global variables
 rm(list = ls())
-path_string <- ".datasets/HFCS/csv/HFCS_UDB_4_0_ASCII/"
+path_stringA <- ".datasets/HFCS/csv/HFCS_UDB_"
+path_stringB <- c("1_5", "2_5", "3_3", "4_0")
+path_year <- c(2011, 2013, 2017, 2020)
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-combined_mean <- c()
+country_mean <- c()
+year_mean <- data.table()
 
-for (selected in country_code) {
-    # Import and measure performance of survey with multiple imputations
-    hfcs <- readRDS(paste0("saves/", selected, "hfcs.RDS"))
-    # aa <- hfcs$designs[[1]]$variables %>% data.table()
+for (i in seq_along(path_stringB)) {
+    path_string <- paste0(path_stringA, path_stringB[i], "_ASCII/")
+    for (selected in country_code) {
+        # Import and measure performance of survey with multiple imputations
+        hfcs <- readRDS(paste0("saves/", selected, "hfcs.RDS"))
+        # aa <- hfcs$designs[[1]]$variables %>% data.table()
 
-    # Combine the mean results from all imputed datasets using Rubin's rules
-    pre <- with(hfcs, svymean(~rentsbi)) %>% MIcombine()
-    combined_mean[[selected]] <- pre$coefficients
+        # Combine the mean results from all imputed datasets using Rubin's rules
+        pre <- with(hfcs, svymean(~rentsbi)) %>% MIcombine()
+        country_mean[[selected]] <- pre$coefficients
+    }
+    # Print the combined mean estimate and its associated standard error
+    country_mean <- country_mean %>%
+        as.data.frame() %>%
+        t()
+    year_mean[, i := country_mean$rentsbi]
 }
-# Print the combined mean estimate and its associated standard error
-combined_mean %>%
-    as.data.frame() %>%
-    t() %>%
-    print()
