f622e9dee275870bdf0a59d53d9247da57fce208
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-14 07:21:35 +0100

    last

--- src/HFCS.main.mi-surveystats copy.R
+++ src/HFCS.main.mi-surveystats copy.R
@@ -10,24 +10,18 @@ path_stringA <- ".datasets/HFCS/csv/HFCS_UDB_"
 path_stringB <- c("1_5", "2_5", "3_3", "4_0")
 path_year <- c(2011, 2013, 2017, 2020)
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-country_mean <- c()
-year_mean <- data.table()
+country_mean <- list()
 
-for (i in seq_along(path_stringB)) {
+for (i in 1) {
     path_string <- paste0(path_stringA, path_stringB[i], "_ASCII/")
     for (selected in country_code) {
         # Import and measure performance of survey with multiple imputations
         hfcs <- readRDS(paste0("saves/HFCS_UDB_", path_stringB[i], "_ASCII/", selected, "hfcs.RDS"))
 
         # Combine the mean results from all imputed datasets using Rubin's rules
-        pre <- with(hfcs, svymean(~rentsbi)) %>% MIcombine()
+        pre <- with(hfcs, svyglm(rentsbi ~ income)) %>% MIcombine()
         country_mean[[selected]] <- pre$coefficients
     }
     # Print the combined mean estimate and its associated standard error
-    country_mean <- country_mean %>% as.data.frame()
-    year_mean <- rbind(year_mean, country_mean)
+    country_mean %>% print()
 }
-year_mean <- year_mean %>% t()
-colnames(year_mean) <- path_year
-year_mean %>% print()
-year_mean %>% fwrite("saves/rentsbi.csv")
