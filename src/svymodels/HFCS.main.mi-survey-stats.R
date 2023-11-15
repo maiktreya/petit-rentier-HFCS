497c1a91d6975947217a00fa91efdede1f89b7db
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-16 00:18:24 +0100

    LAST

--- src/svymodels/HFCS.main.mi-survey-stats.R
+++ src/svymodels/HFCS.main.mi-survey-stats.R
@@ -11,14 +11,25 @@ path_stringA <- ".datasets/HFCS/csv/HFCS_UDB_"
 path_stringB <- c("1_5", "2_5", "3_3", "4_0")
 path_year <- c(2011, 2013, 2017, 2020)
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-var_code <- c("rentsbi", "income")
+var_code <- c(
+    "age_ref", "hsize", "edu_ref", "head_gendr", "employm", "tenan",
+    "rental", "financ", "pvpens", "pvtran", "income",
+    "net_we", "net_fi", "other", "main", "real", "bussiness", "total_real",
+    "num_bs", "val_op", "num_op", "status", "d_isco", "d_nace",
+    "SA0100", "HW0010.x"
+)
+
+
+## LEVEL 1: ITERATE AMONG VARIABLES
 for (varname in var_code) {
     year_mean <- data.table() # define clean object
 
+    # LEVEL 2: ITERATE AMONG ANNUAL WAVES
     for (i in seq_along(path_stringB)) {
         country_mean <- c() # define clean object
         path_string <- paste0(path_stringA, path_stringB[i], "_ASCII/")
 
+        # LEVEL 3: ITERATE AMONG COUNTRIES
         for (selected in country_code) {
             # Import and measure performance of survey with multiple imputations
             hfcs <- paste0("saves/HFCS_UDB_", path_stringB[i], "_ASCII/", selected, "hfcs.RDS") %>% readRDS()
