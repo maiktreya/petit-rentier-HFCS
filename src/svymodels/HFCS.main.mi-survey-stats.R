daa6474a48507a794f0f02c584a36661b247468c
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-16 00:15:30 +0100

    last

--- src/svymodels/HFCS.main.mi-survey-stats.R
+++ src/svymodels/HFCS.main.mi-survey-stats.R
@@ -11,24 +11,30 @@ path_stringA <- ".datasets/HFCS/csv/HFCS_UDB_"
 path_stringB <- c("1_5", "2_5", "3_3", "4_0")
 path_year <- c(2011, 2013, 2017, 2020)
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-country_mean <- c()
-year_mean <- data.table()
-varname <- "rentsbi"
-for (i in seq_along(path_stringB)) {
-    path_string <- paste0(path_stringA, path_stringB[i], "_ASCII/")
-    for (selected in country_code) {
-        # Import and measure performance of survey with multiple imputations
-        hfcs <- readRDS(paste0("saves/HFCS_UDB_", path_stringB[i], "_ASCII/", selected, "hfcs.RDS"))
+var_code <- c("rentsbi", "income")
+for (varname in varcode) {
+    year_mean <- data.table() # define clean object
 
-        # Combine the mean results from all imputed datasets using Rubin's rules
-        pre <- with(hfcs, svymean(as.formula(paste0("~", varname)))) %>% MIcombine()
-        country_mean[[selected]] <- pre$coefficients
+    for (i in seq_along(path_stringB)) {
+        country_mean <- c() # define clean object
+        path_string <- paste0(path_stringA, path_stringB[i], "_ASCII/")
+
+        for (selected in country_code) {
+            # Import and measure performance of survey with multiple imputations
+            hfcs <- paste0("saves/HFCS_UDB_", path_stringB[i], "_ASCII/", selected, "hfcs.RDS") %>% readRDS()
+
+            # Combine the mean results from all imputed datasets using Rubin's rules
+            pre <- with(hfcs, svymean(as.formula(paste0("~", varname)))) %>% MIcombine()
+            country_mean[selected] <- pre$coefficients
+        }
+        # Print the combined mean estimate and its associated standard error
+        year_mean <- cbind(year_mean, country_mean)
     }
-    # Print the combined mean estimate and its associated standard error
-    country_mean <- country_mean %>% as.data.frame()
-    year_mean <- rbind(year_mean, country_mean)
+    # tidy and export the result
+    colnames(year_mean) <- path_year %>% as.character()
+    year_mean %>% print()
+    fwrite(year_mean, paste0("saves/", varname, ".csv"))
+
+    # clean enviroment for next iteration in loop
+    rm(list = c("hfcs", "pre", "country_mean", "year_mean"))
 }
-year_mean <- year_mean %>% t()
-colnames(year_mean) <- path_year
-year_mean %>% print()
-year_mean %>% fwrite(paste0("saves/", varname, ".csv"))
