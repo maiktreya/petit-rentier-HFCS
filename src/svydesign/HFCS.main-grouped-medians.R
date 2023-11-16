9e3c70da615090b52ab51e46b0cff3125baf708d
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-16 17:09:26 +0100

    last

--- src/svydesign/HFCS.main-grouped-medians.R
+++ src/svydesign/HFCS.main-grouped-medians.R
@@ -79,7 +79,7 @@ for (varname in var_code) {
 
             # Loop through each svydesign object and calculate the mean of HB0100
             # for (i in 1:5) means[i] <- svymean(~rentsbi, designs[[i]], na.rm = TRUE)#
-            for (i in 1:5) means[i] <- svymean(as.formula(paste0("~", varname)), designs[[i]])[1] %>% unname()
+            for (i in 1:5) means[i] <- svyquantile(as.formula(paste0("~", varname)), designs[[i]], quantiles = c(.5))[1] %>% unname()
 
             # Calculate the average mean across all imputations
             mean_of_means[n] <- mean(means) %>% print()
