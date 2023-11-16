1170fec756184e77ffc2b07fca2b969900d97a38
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-16 23:27:13 +0100

    last

--- src/lme4/hybrid-mixed-mean.R
+++ src/lme4/hybrid-mixed-mean.R
@@ -12,10 +12,21 @@ outcome <- fread("output/MEANS/rentsbi.csv", header = TRUE) %>%
 tenan <- fread("output/MEANS/tenan.csv", header = TRUE) %>%
     unlist() %>%
     as.vector()
-
+rental_share <- fread("output/MEANS/rental-share.csv", header = TRUE) %>%
+    unlist() %>%
+    as.vector()
+finan_share <- fread("output/MEANS/financ-share.csv", header = TRUE) %>%
+    unlist() %>%
+    as.vector()
 group <- rep(countries, 4)
 time <- as.vector(cbind(rep(1, 15), rep(2, 15), rep(3, 15), rep(4, 15)))
 
 dataset <- data.table(group, time, outcome)
-model <- lmer(outcome ~ time + tenan + (1 + time | group), data = dataset)
-summary(model) %>% print()
+modelA <- lmer(outcome ~ time + tenan + (1 + time | group), data = dataset)
+modelB <- lmer(outcome ~ time + tenan + (0 + time | group), data = dataset)
+modelC <- lmer(outcome ~ time + tenan + rental_share + finan_share + (1 + time | group), data = dataset)
+modelD <- lmer(outcome ~ time + tenan + rental_share + finan_share + (0 + time | group), data = dataset)
+summary(modelA) %>% print()
+summary(modelB) %>% print()
+summary(modelC) %>% print()
+summary(modelD) %>% print()
