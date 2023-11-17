596fcce48b6e38ad4aa538c5e2997058ae74b6c2
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-17 22:13:54 +0100

    last

--- src/lme4/hybrid-mixed-mean.R
+++ src/lme4/hybrid-mixed-mean.R
@@ -9,6 +9,12 @@ countries <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL",
 outcome <- fread("output/MEANS/rentsbi.csv", header = TRUE) %>%
     unlist() %>%
     as.vector()
+outcome2 <- fread("output/MEANS/rentsbi2.csv", header = TRUE) %>%
+    unlist() %>%
+    as.vector()
+outcome5 <- fread("output/MEANS/rentsbi5.csv", header = TRUE) %>%
+    unlist() %>%
+    as.vector()
 tenan <- fread("output/MEANS/tenan.csv", header = TRUE) %>%
     unlist() %>%
     as.vector()
@@ -21,10 +27,10 @@ finan_share <- fread("output/MEANS/financ-share.csv", header = TRUE) %>%
 group <- rep(countries, 4)
 time <- as.vector(cbind(rep(1, 15), rep(2, 15), rep(3, 15), rep(4, 15)))
 
-dataset <- data.table(group, time, outcome)
-modelA <- lmer(outcome ~ time + (1 | group), data = dataset)
+dataset <- data.table(group, time, outcome, outcome2, outcome5)
+modelA <- lmer(outcome2 ~ time + (1 + time | group), data = dataset)
 modelB <- lmer(outcome ~ time + (1 + time | group), data = dataset)
-modelC <- lmer(outcome ~ time + (0 + time | group), data = dataset)
+modelC <- lmer(outcome5 ~ time + (0 + time | group), data = dataset)
 
 summary(modelA) %>% print()
 summary(modelB) %>% print() # best
