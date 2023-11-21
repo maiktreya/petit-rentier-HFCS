891d5ccb37c3a254c6be88540234502cb616a24f
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-21 17:32:25 +0100

    last

--- src/svy/svydesign/HFCS.main-grouped-means.R
+++ src/svy/svydesign/HFCS.main-grouped-means.R
@@ -11,8 +11,8 @@ path_stringA <- ".datasets/HFCS/csv/HFCS_UDB_"
 path_stringB <- c("1_6", "2_5", "3_3", "4_0")
 path_year <- c(2011, 2013, 2017, 2020)
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-var_code <- c("rentsbi", "rentsbi2", "rentsbi5")
-prefix <- "rent-fin-pro.svy/"
+var_code <- c("profit", "Kgains", "rental", "financ")
+prefix <- ""
 count <- 0
 
 for (varname in var_code) {
