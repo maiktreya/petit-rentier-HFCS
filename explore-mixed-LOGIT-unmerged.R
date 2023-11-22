ddbb3601d4e588c193e9535c1bdf05f3c8bf4050
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-23 00:02:23 +0100

    last

--- explore-mixed-LOGIT-unmerged.R
+++ explore-mixed-LOGIT-unmerged.R
@@ -5,10 +5,10 @@ library(data.table)
 library(lme4)
 library(survey)
 library(mice)
+library(mitools)
 
 # clean enviroment
 rm(list = ls())
-amplified <- FALSE
 # import and merrge multicountry HFCS waves
 datasetA <- fread(".datasets/HFCSgz/1_6.gz", header = TRUE)[, wave := 1]
 datasetB <- fread(".datasets/HFCSgz/2_5.gz", header = TRUE)[, wave := 2]
@@ -16,7 +16,8 @@ datasetC <- fread(".datasets/HFCSgz/3_3.gz", header = TRUE)[, wave := 3]
 datasetD <- fread(".datasets/HFCSgz/4_0.gz", header = TRUE)[, wave := 4]
 dataset <- rbind(datasetA, datasetB, datasetC, datasetD)
 rm(list = setdiff(ls(), "dataset"))
-model <- list()
+model <- dataset_s <- list()
+amplified <- FALSE
 
 dataset$class <- dataset$employm %>%
     factor(levels = c(1, 2, 3, 4, 5), labels = c("Employee", "Self-employed", "Unemployed", "Retired", "Other"))
@@ -27,17 +28,14 @@ dataset$edu_ref <- dataset$edu_ref %>%
 dataset$head_gendr <- dataset$head_gendr %>%
     factor(levels = c(1, 2), labels = c("male", "female"))
 
+dataset <- dataset[, list(rentsbi, wave, hsize, head_gendr, age_ref, sa0100, implicate)]
+
+for (i in 1:5) dataset_s[[i]] <- dataset[implicate == i]
+dataset <- imputationList(dataset)
+model <- with(dataset_s, glmer(rentsbi ~ wave + hsize + head_gendr + age_ref + (1 | sa0100), family = binomial))
 
 ############################################################################################### 3
 # STEP 1: Isolate implicate and test the mixed model
-
-for (i in 1:5) {
-    start_time <- Sys.time()
-    dataset_s <- dataset[implicate == i]
-    model[[i]] <- glmer(rentsbi ~ wave + hsize + head_gendr + age_ref + class + edu_ref + (1 | sa0100), data = dataset_s, family = binomial)
-    (start_time - Sys.time()) %>% print()
-}
-
 
 if (amplified == TRUE) {
     # STEP 2: pool, estimations
