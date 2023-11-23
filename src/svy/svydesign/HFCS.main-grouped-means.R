aefed0fb8275334916076859f2ff0747e244fd8d
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-23 19:48:04 +0100

    last

--- src/svy/svydesign/HFCS.main-grouped-means.R
+++ src/svy/svydesign/HFCS.main-grouped-means.R
@@ -11,7 +11,7 @@ path_stringA <- ".datasets/HFCS/csv/HFCS_UDB_"
 path_stringB <- c("1_6", "2_5", "3_3", "4_0")
 path_year <- c(2011, 2013, 2017, 2020)
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-var_code <- c("rental")
+var_code <- c("rentsbi")
 prefix <- ""
 count <- 0
 
@@ -62,6 +62,13 @@ for (varname in var_code) {
                     )
                 ]
                 transf[, (varname) := suppressWarnings(as.numeric(get(varname)))][, (varname) := ifelse(is.na(get(varname)), 0, get(varname))]
+                transf[, income := suppressWarnings(as.numeric(income))][, income := ifelse(is.na(income), 0, income)]
+                transf[, financ := suppressWarnings(as.numeric(financ))][, financ := ifelse(is.na(financ), 0, financ)]
+                transf[, profit := suppressWarnings(as.numeric(profit))][, profit := ifelse(is.na(profit), 0, profit)]
+                transf[, rental := suppressWarnings(as.numeric(rental))][, rental := ifelse(is.na(rental), 0, rental)]
+                transf[, rentsbi := 0][income > 0 & ((financ + profit + rental) / income) > 0.1, rentsbi := 1]
+                transf[, rentsbi5 := 0][income > 0 & ((financ + profit + rental) / income) > 0.05, rentsbi5 := 1]
+                transf[, rentsbi2 := 0][income > 0 & ((financ + profit + rental) / income) > 0.02, rentsbi2 := 1]
                 transf[employm %in% c(1, 3), employm := 1] # worker
                 transf[!(employm %in% c(1, 2, 3)), employm := NA] # retired/other
                 transf[status == 2 & employm == 3, employm := 2] # self-employed
