c2b5de6fc36a12990d27fd6a191654d067dba228
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-28 22:44:01 +0100

    last

--- explore-panelFE.R
+++ explore-panelFE.R
@@ -9,13 +9,13 @@ library(plm)
 rm(list = ls())
 
 countries <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-outcome <- fread("output/MEANS/class/rentsbi.csv", header = TRUE) %>%
+outcome <- fread("output/MEANS/ren-fin-pro-pens/rentsbi.csv", header = TRUE) %>%
     unlist() %>%
     as.vector()
-outcome2 <- fread("output/MEANS/class/rentsbi2.csv", header = TRUE) %>%
+outcome2 <- fread("output/MEANS/ren-fin-pro-pens/rentsbi2.csv", header = TRUE) %>%
     unlist() %>%
     as.vector()
-outcome5 <- fread("output/MEANS/class/rentsbi5.csv", header = TRUE) %>%
+outcome5 <- fread("output/MEANS/ren-fin-pro-pens/rentsbi5.csv", header = TRUE) %>%
     unlist() %>%
     as.vector()
 tenan <- fread("output/MEANS/tenan.csv", header = TRUE) %>%
