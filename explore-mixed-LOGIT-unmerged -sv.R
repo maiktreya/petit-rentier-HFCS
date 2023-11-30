d3105d716ca266999087f12fc72c0426dac60886
il    iciuv <miguel.garcia.duch@gmail.com>
2023-11-30 03:56:29 +0100

    last

--- explore-mixed-LOGIT-unmerged -sv.R
+++ explore-mixed-LOGIT-unmerged -sv.R
@@ -23,8 +23,6 @@ rm(list = setdiff(ls(), "dataset"))
 model <- dataset_s <- list()
 amplified <- FALSE
 n_imputations <- 5
-
-
 dataset[employm %in% c(1, 3), employm := 1] # worker
 dataset[!(employm %in% c(1, 2, 3)), employm := NA] # retired/other
 dataset[status == 2 & employm == 2, employm := 2] # capitalist
@@ -35,46 +33,35 @@ dataset[retired_status == 1, employm := 1] # worker
 dataset[retired_status == 2, employm := 2] # capitalist
 dataset[retired_status == 3, employm := 3] # self-employed
 dataset[retired_isco08 %in% c(10, 11, 12, 13, 14, 15, 16, 17, 18, 19), employm := 4] # manager
-
 dataset[age_ref < 30, age := 1][age_ref >= 30 & age_ref < 50, age := 2][age_ref >= 50 & age_ref < 70, age := 3][age_ref >= 70, age := 4]
-
 dataset[quintile.gwealth != 5, quintile.gwealth := 1][quintile.gwealth == 5, quintile.gwealth := 2] # top wealth quintile
 dataset[quintile.gincome != 5, quintile.gincome := 1][quintile.gincome == 5, quintile.gincome := 2] # top income quintile
-
 dataset[edu_ref %in% c(2, 3, 4), edu_ref := 2][edu_ref %in% c(5, 6), edu_ref := 3] # c("primary", "low-sec", "mid-sec", "high_sec", "low-ter", "high-ter")
 # dataset[employm %in% c(2, 3), employm := 3]
-
 dataset$age <- dataset$age %>%
     factor(levels = c(2, 1, 3, 4), labels = c("30-49", "0-29", "50-69", "+70"))
-
 dataset$class <- dataset$employm %>%
-    factor(levels = c(1, 2, 3, 4, 5), labels = c("Worker", "Employer", "Self-Employed", "Manager", "Inactive"))
-
+    factor(levels = c(5, 2, 3, 4, 1), labels = c("Inactive", "Employer", "Self-Employed", "Manager", "Worker"))
 dataset$edu_ref <- dataset$edu_ref %>%
     factor(levels = c(1, 2, 3), labels = c("primary", "secondary", "tertiary"))
-
 dataset$head_gendr <- dataset$head_gendr %>%
     factor(levels = c(1, 2), labels = c("male", "female"))
-
 dataset$quintile.gwealth <- dataset$quintile.gwealth %>%
     factor(levels = c(1, 2), labels = c("non-top-wealth", "top-wealth"))
-
 dataset$quintile.gincome <- dataset$quintile.gincome %>%
     factor(levels = c(1, 2), labels = c("non-top-income", "top-income"))
-
 
 for (i in 1:5) {
     start_time <- Sys.time()
     dataset_s <- dataset[implicate == i]
     model[[i]] <- glmer(
-        rentsbi ~ hsize + head_gendr + age + edu_ref + quintile.gincome + quintile.gwealth + class + class:wave + (1 | sa0100) + (1 | wave),
+        rentsbi ~ hsize + head_gendr + age + edu_ref + quintile.gincome + quintile.gwealth + class + (1 | sa0100) + (1 | wave),
         family = binomial,
         data = dataset_s
     )
 
     (start_time - Sys.time()) %>% print()
 }
-
 
 coef_estimates <- lapply(model, function(m) fixef(m))
 se_estimates <- lapply(model, function(m) sqrt(diag(vcov(m))))
