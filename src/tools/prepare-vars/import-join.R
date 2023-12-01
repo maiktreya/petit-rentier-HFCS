156c5dd33e75c072f5ce8e9de4dcfe82e41bf2f8
il    iciuv <miguel.garcia.duch@gmail.com>
2023-12-01 18:31:55 +0100

    last

--- src/tools/prepare-vars/import-join.R
+++ src/tools/prepare-vars/import-join.R
@@ -1,4 +1,16 @@
 # HFCS  cDATA PREPARATION each file contains previously: 1) merged  household + personal + derived files 2) rbinded 5 implicates
+varnames <- c(
+    "profit", "Kgains", "quintile.gwealth", "quintile.gincome",
+    "age_ref", "hsize", "edu_ref", "head_gendr", "employm", "tenan",
+    "rental", "financ", "pvpens", "pvtran", "income",
+    "net_we", "net_fi", "other", "main", "real", "bussiness", "total_real",
+    "num_bs", "val_op", "num_op", "status", "d_isco", "d_nace", "retired_status", "retired_isco08",
+    "sa0010", "sa0100", "hw0010.x"
+)
+countries_wave_1 <- c("BE", "DE", "ES", "FR", "PT", "SI", "LU", "MT", "GR", "NL", "CY", "IT", "SK", "AT", "FI")
+countries_wave_2 <- c("DE", "ES", "FR", "PT", "IE", "NL", "CY", "IT", "SI", "MT", "PL", "LU", "AT", "SK", "EE", "FI", "GR", "LV", "HU", "BE")
+countries_wave_3 <- c("ES", "IE", "DE", "PT", "SI", "IT", "CY", "LT", "HR", "LU", "MT", "AT", "SK", "FI", "NL", "GR", "HU", "LV", "PL", "EE", "FR", "BE")
+countries_wave_4 <- c("ES", "LT", "IE", "PT", "DE", "SI", "IT", "CY", "HR", "AT", "HU", "SK", "FI", "GR", "NL", "LU", "LV", "MT", "EE", "FR", "BE", "CZ")
 # all has been previously optimized to gz files for storage and read spead with data.table
 
 library(magrittr) # for piping without dplyr
@@ -10,10 +22,7 @@ datasetA <- fread(".datasets/HFCSgz/1_6.gz", header = TRUE)[, wave := 1][sa0100 
 datasetB <- fread(".datasets/HFCSgz/2_5.gz", header = TRUE)[, wave := 2]
 datasetC <- fread(".datasets/HFCSgz/3_3.gz", header = TRUE)[, wave := 3]
 datasetD <- fread(".datasets/HFCSgz/4_0.gz", header = TRUE)[, wave := 4]
-countries_wave_1 <- c("BE", "DE", "ES", "FR", "PT", "SI", "LU", "MT", "GR", "NL", "CY", "IT", "SK", "AT", "FI")
-countries_wave_2 <- c("DE", "ES", "FR", "PT", "IE", "NL", "CY", "IT", "SI", "MT", "PL", "LU", "AT", "SK", "EE", "FI", "GR", "LV", "HU", "BE")
-countries_wave_3 <- c("ES", "IE", "DE", "PT", "SI", "IT", "CY", "LT", "HR", "LU", "MT", "AT", "SK", "FI", "NL", "GR", "HU", "LV", "PL", "EE", "FR", "BE")
-countries_wave_4 <- c("ES", "LT", "IE", "PT", "DE", "SI", "IT", "CY", "HR", "AT", "HU", "SK", "FI", "GR", "NL", "LU", "LV", "MT", "EE", "FR", "BE", "CZ")
+
 dataset <- rbind(datasetA, datasetB, datasetC, datasetD)
 
 # remove unneeded so we avoid out of memory problems with large datasets
