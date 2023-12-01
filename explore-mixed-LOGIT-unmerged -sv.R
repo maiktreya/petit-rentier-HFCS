52bb9a618426ee210197033f72fb4411b53e79e4
il    iciuv <miguel.garcia.duch@gmail.com>
2023-12-01 16:51:47 +0100

    last

--- explore-mixed-LOGIT-unmerged -sv.R
+++ explore-mixed-LOGIT-unmerged -sv.R
@@ -27,6 +27,8 @@ for (i in 1:5) {
         weights = weights,
         control = glmerControl(
             optimizer = "bobyqa", # bobyqa, Nelder_Mead, nloptwrap,optim  method='nlminb'
+            boundary.tol = 1e-5,
+            calc.derivs = FALSE,
             optCtrl = list(maxfun = 2e5)
         ),
         verbose = 2,
