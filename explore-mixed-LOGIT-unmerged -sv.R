eb8dea0e7d648666122e6c0f0fa3d2511ff9e9ce
il    iciuv <miguel.garcia.duch@gmail.com>
2023-12-01 03:35:26 +0100

    last

--- explore-mixed-LOGIT-unmerged -sv.R
+++ explore-mixed-LOGIT-unmerged -sv.R
@@ -24,14 +24,14 @@ for (i in 1:5) {
         rentsbi ~ hsize + head_gendr + age + edu_ref + quintile.gincome + quintile.gwealth + class + (1 | sa0100) + (1 | wave),
         family = binomial,
         data = dataset_s,
-        weights = hw0010.x,
+        weights = weights,
         control = glmerControl(
             optimizer = "Nelder_Mead", # bobyka, Nelder_Mead, nloptwrap, nlminb, optim
             optCtrl = list(maxfun = 2e5),
             standardize.X = FALSE
         ),
-        verbose = TRUE,
-        nAGQ = 0
+        verbose = 2,
+        nAGQ = 1
     )
     (start_time - Sys.time()) %>% print()
 }
@@ -68,4 +68,4 @@ p_values <- 2 * pt(-abs(t_stats), df = (n_imputations - 1))
 combined_results <- cbind(names = names(fixef(model[[1]])), mean_estimates, combined_se, t_stats, p_values) %>% print()
 
 # Export joint results to csv
-fwrite(combined_results, "output/MODELS/MICRO/complete.csv")
+fwrite(combined_results, "output/MODELS/MICRO/ren-fin/w-complete.csv")
