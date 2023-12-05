2b50f6d7496ed52faecd77b507f7a20ea5413165
il    iciuv <miguel.garcia.duch@gmail.com>
2023-12-05 21:57:43 +0100

    last

--- src/svy/svydesign/HFCS.main-grouped-means.R
+++ src/svy/svydesign/HFCS.main-grouped-means.R
@@ -11,7 +11,7 @@ path_stringA <- ".datasets/HFCS/csv/HFCS_UDB_"
 path_stringB <- c("1_6", "2_5", "3_3", "4_0")
 path_year <- c(2011, 2013, 2017, 2020)
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-var_code <- c("rentsbi", "rentsbi5")
+var_code <- c("iscapital")
 prefix <- ""
 count <- 0
 
@@ -114,7 +114,7 @@ for (varname in var_code) {
         mean_of_years <- cbind(mean_of_years, mean_of_means) %>% print()
     }
     colnames(mean_of_years) <- path_year %>% as.character()
-    fwrite(mean_of_years, paste0("output/MEANS/highincome/", prefix, varname, "-pens.csv"))
+    # fwrite(mean_of_years, paste0("output/MEANS/highincome/", prefix, varname, "-pens.csv"))
     paste("variable", varname, "sucessfully exported.", (start_time - Sys.time()), "have passed in execution.") %>%
         print()
 }
