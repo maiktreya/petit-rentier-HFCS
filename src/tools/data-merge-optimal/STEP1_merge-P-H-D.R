15c0de6d4a8aa69dfc2e9d99e34a096734e6757a
il    iciuv <miguel.garcia.duch@gmail.com>
2024-01-16 18:36:26 +0100

    last

--- src/tools/data-merge-optimal/STEP1_merge-P-H-D.R
+++ src/tools/data-merge-optimal/STEP1_merge-P-H-D.R
@@ -16,9 +16,10 @@ for (wave in path_stringB) {
         "dhaq01ea", "dhiq01ea",
         "dhageh1", "dh0001", "dheduh1", "dhgenderh1", "dhemph1", "dhhst",
         "di1300", "di1400", "di1520", "di1700", "di2000",
-        "dn3001", "da2100", "da1120", "da1110", "da1400", "da1200", "da1000"
+        "dn3001", "da2100", "da1120", "da1110", "da1400", "da1200", "da1000",
+        "da1110i", "da1121i", "da1122i"
     )
-    selnamesH <- c("hd0210", "hb2900", "hb2410", "hg0510", "hg0610", "hg0310")
+    selnamesH <- c("hd0210", "hb2900", "hb2410", "hg0510", "hg0610", "hg0310", "hd1300", "hd1400", "hd1500", "hd1600", "hd1900")
     common <- c("sa0010", "sa0100", "im0100")
 
     # JOINT MATRIX PRE SUMMING IMPUTATIONS (YEAR-WAVE)
@@ -38,14 +39,16 @@ for (wave in path_stringB) {
                 "dhageh1", "dh0001", "dheduh1", "dhgenderh1", "dhemph1", "dhhst",
                 "hg0310", "di1400", "di1520", "di1700", "di2000",
                 "dn3001", "da2100", "da1120", "da1110", "da1400", "da1200", "da1000",
-                "hd0210", "hb2900", "hb2410", "pe0200", "pe0300", "pe0400", "fpe0200", "fpe0300"
+                "hd0210", "hb2900", "hb2410", "pe0200", "pe0300", "pe0400", "fpe0200", "fpe0300",
+                "da1110i", "da1121i", "da1122i", "hd1300", "hd1400", "hd1500", "hd1600", "hd1900"
             ),
             new = c(
                 "profit", "Kgains", "quintile.gwealth", "quintile.gincome",
                 "age_ref", "hsize", "edu_ref", "head_gendr", "employm", "tenan",
                 "rental", "financ", "pvpens", "pvtran", "income",
                 "net_we", "net_fi", "other", "main", "real", "bussiness", "total_real",
-                "num_bs", "val_op", "num_op", "status", "d_isco", "d_nace", "retired_status", "retired_isco08"
+                "num_bs", "val_op", "num_op", "status", "d_isco", "d_nace", "retired_status", "retired_isco08",
+                "homeown", "otherpB", "otherpN", "mutual", "bonds", "shares", "managed", "other"
             )
         )
         transf <- transf[
@@ -56,10 +59,10 @@ for (wave in path_stringB) {
                 "rental", "financ", "pvpens", "pvtran", "income",
                 "net_we", "net_fi", "other", "main", "real", "bussiness", "total_real",
                 "num_bs", "val_op", "num_op", "status", "d_isco", "d_nace", "retired_status", "retired_isco08",
+                "homeown", "otherpB", "otherpN", "mutual", "bonds", "shares", "managed", "other",
                 "sa0010", "sa0100", "hw0010.x"
             )
         ]
-
 
         country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
         medians_real <- fread("output/MEDIANS/real.csv", header = TRUE)
@@ -73,15 +76,18 @@ for (wave in path_stringB) {
             transf[sa0100 == country_code[n], quintile.fwealth := 1][net_fi >= ind_median_fin, quintile.fwealth := 2]
         }
 
-
         # fix germany character values in income series.
         transf[, income := suppressWarnings(as.numeric(income))][, income := ifelse(is.na(income), 0, income)]
+        transf[, pvpens := suppressWarnings(as.numeric(pvpens))][, pvpens := ifelse(is.na(pvpens), 0, pvpens)]
         transf[, financ := suppressWarnings(as.numeric(financ))][, financ := ifelse(is.na(financ), 0, financ)]
         transf[, profit := suppressWarnings(as.numeric(profit))][, profit := ifelse(is.na(profit), 0, profit)]
         transf[, rental := suppressWarnings(as.numeric(rental))][, rental := ifelse(is.na(rental), 0, rental)]
         transf[, rentsbi := 0][income > 0 & ((financ + rental) / income) > 0.1, rentsbi := 1]
-        transf[, rentsbi5 := 0][income > 0 & ((financ + rental) / income) > 0.05, rentsbi5 := 1]
         transf[, rentsbi20 := 0][income > 0 & ((financ + rental) / income) > 0.2, rentsbi20 := 1]
+        transf[, rentsbi_pens := 0][income > 0 & ((financ + rental + pvpens) / income) > 0.1, rentsbi_pens := 1]
+        transf[, rentsbi20_pens := 0][income > 0 & ((financ + rental + pvpens) / income) > 0.2, rentsbi20_pens := 1]
+        transf[, rents_mean := (financ + rental)]
+        transf[, rents_mean_pens := (financ + rental + pvpens)]
         imp[[m]] <- transf[, implicate := m]
     }
     imp <- rbindlist(imp)
