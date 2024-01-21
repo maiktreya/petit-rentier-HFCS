54f9565e545e57d5421b06a5158f4b87ead64e6d
il    iciuv <miguel.garcia.duch@gmail.com>
2024-01-21 02:19:40 +0100

    last

--- src/main/explore-plot.R
+++ src/main/explore-plot.R
@@ -1,13 +1,54 @@
+# Get histograms and empirical distribution of  variables from weighted surveys
+rm(list = ls()) # clean enviroment
+
+# neeeded libraries
 library(data.table)
 library(magrittr)
+library(survey)
+library(ggplot2)
 
-rm(list = ls())
-datamatrix <- fread("output/MEANS/ren-fin-pro/rentsbi.csv", header = TRUE)
-datamatrix2 <- fread("output/MEANS/ren-fin-pro/rentsbi5.csv", header = TRUE)
-
+# source main dataset and define global variables
+source("src/tools/prepare-vars/import-join.R")
 country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-dataset <- cbind(country_code, datamatrix) %>% print()
-dataset2 <- cbind(country_code, datamatrix2) %>% print()
+data_implicate <- list()
+varname <- "rents_mean"
 
-test <- dataset[country_code == "AT"][, 2:5] %>% unname()
-plot(test)
+# convert to survey design to account for weights
+for (i in 1:5) {
+    # Create the svydesign object for the i-th imputation
+    prep_data <- dataset[implicate == i]
+    data_implicate[[i]] <- svydesign(
+        ids = ~1,
+        weights = ~hw0010.x,
+        strata = ~sa0100,
+        data = prep_data
+    )
+}
+
+# select implicate and country
+for (n in country_code[6]) {
+    national_data1 <- subset(data_implicate[[1]], sa0100 == n & wave == 1)
+    national_data2 <- subset(data_implicate[[1]], sa0100 == n & wave == 4)
+}
+
+# define limits to trim outliers
+upper1 <- svyquantile(as.formula(paste0("~", varname)), national_data1, quantiles = .95, na.rm = TRUE)[1][[1]][1]
+lower1 <- svyquantile(as.formula(paste0("~", varname)), national_data1, quantiles = .5, na.rm = TRUE)[1][[1]][1]
+
+# get trimmed empirical distributions at first and last period
+df_time1 <- svysmooth(~rents_mean, design = subset(national_data1, rents_mean < upper1 & rents_mean > 0))[[1]] %>% as.data.frame()
+df_time2 <- svysmooth(~rents_mean, design = subset(national_data2, rents_mean < upper2 & rents_mean > 0))[[1]] %>% as.data.frame()
+
+# STEP 1: Plot densities using ggplot2
+chart <-
+    ggplot() +
+    geom_step(data = df_time1, aes(x = x, y = y), color = "blue") +
+    geom_step(data = df_time2, aes(x = x, y = y), color = "red") +
+    labs(title = "Comparative ECDFs at Two Time Points", x = "Variable", y = "ECDF") +
+    theme_minimal() %>% print()
+
+# STEP 2: Plot cumulative densities using standard library
+df_cdf <- svycdf(~rents_mean, design = subset(national_data1, rents_mean < upper1 & rents_mean > 0))
+df_ecdf <- ecdf(subset(national_data2, rents_mean < upper2 & rents_mean > 0)$variables[, "rents_mean"]$rents_mean)
+df_cdf[[1]] %>% plot()
+lines(df_ecdf, col.points = "red")
--- tests.R
+++ tests.R
@@ -1,54 +1,24 @@
-# Get histograms and empirical distribution of  variables from weighted surveys
-rm(list = ls()) # clean enviroment
+data(api)
+dstrat <- svydesign(
+    id = ~1, strata = ~stype, weights = ~pw, data = apistrat,
+    fpc = ~fpc
+)
+cdf.est <- svycdf(~ enroll + api00 + api99, dstrat)
+cdf.est
+## function
+cdf.est[[1]]
+## evaluate the function
+cdf.est[[1]](800)
+cdf.est[[2]](800)
 
-# neeeded libraries
-library(data.table)
-library(magrittr)
-library(survey)
-library(ggplot2)
+## compare to population and sample CDFs.
+opar <- par(mfrow = c(2, 1))
+cdf.pop <- ecdf(apipop$enroll)
+cdf.samp <- ecdf(apistrat$enroll)
+plot(cdf.pop, main = "Population vs sample", xlab = "Enrollment")
+lines(cdf.samp, col.points = "red")
 
-# source main dataset and define global variables
-source("src/tools/prepare-vars/import-join.R")
-country_code <- c("AT", "BE", "CY", "FI", "FR", "DE", "GR", "IT", "LU", "MT", "NL", "PT", "SI", "SK", "ES")
-data_implicate <- list()
-varname <- "rents_mean"
+plot(cdf.pop, main = "Population vs estimate", xlab = "Enrollment")
+lines(cdf.est[[1]], col.points = "red")
 
-# convert to survey design to account for weights
-for (i in 1:5) {
-    # Create the svydesign object for the i-th imputation
-    prep_data <- dataset[implicate == i]
-    data_implicate[[i]] <- svydesign(
-        ids = ~1,
-        weights = ~hw0010.x,
-        strata = ~sa0100,
-        data = prep_data
-    )
-}
-
-# select implicate and country
-for (n in country_code[6]) {
-    national_data1 <- subset(data_implicate[[1]], sa0100 == n & wave == 1)
-    national_data2 <- subset(data_implicate[[1]], sa0100 == n & wave == 4)
-}
-
-# define limits to trim outliers
-upper1 <- svyquantile(as.formula(paste0("~", varname)), national_data1, quantiles = .95, na.rm = TRUE)[1][[1]][1]
-lower1 <- svyquantile(as.formula(paste0("~", varname)), national_data1, quantiles = .5, na.rm = TRUE)[1][[1]][1]
-
-# get trimmed empirical distributions at first and last period
-df_time1 <- svysmooth(~rents_mean, design = subset(national_data1, rents_mean < upper1 & rents_mean > 0))[[1]] %>% as.data.frame()
-df_time2 <- svysmooth(~rents_mean, design = subset(national_data2, rents_mean < upper2 & rents_mean > 0))[[1]] %>% as.data.frame()
-
-# STEP 1: Plot densities using ggplot2
-chart <-
-    ggplot() +
-    geom_step(data = df_time1, aes(x = x, y = y), color = "blue") +
-    geom_step(data = df_time2, aes(x = x, y = y), color = "red") +
-    labs(title = "Comparative ECDFs at Two Time Points", x = "Variable", y = "ECDF") +
-    theme_minimal() %>% print()
-
-# STEP 2: Plot cumulative densities using standard library
-df_cdf <- svycdf(~rents_mean, design = subset(national_data1, rents_mean < upper1 & rents_mean > 0))
-df_ecdf <- ecdf(subset(national_data2, rents_mean < upper2 & rents_mean > 0)$variables[, "rents_mean"]$rents_mean)
-df_cdf[[1]] %>% plot()
-lines(df_ecdf, col.points = "red")
+par(opar)
