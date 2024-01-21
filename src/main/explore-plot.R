2257b75b77375097f075acdd88a95024d9ecd844
il    iciuv <miguel.garcia.duch@gmail.com>
2024-01-21 08:49:03 +0100

    last

--- src/main/explore-plot.R
+++ src/main/explore-plot.R
@@ -38,8 +38,8 @@ for (n in country_code) {
     national_data2 <- subset(data_implicate[[1]], sa0100 == n & wave == 4)
 
     # define limits to trim outliers
-    upper1 <- svyquantile(~(rents_mean / income), national_data1, quantiles = .99, na.rm = TRUE)[1][[1]][1]
-    upper2 <- svyquantile(~(rents_mean / income), national_data2, quantiles = .99, na.rm = TRUE)[1][[1]][1]
+    upper1 <- svyquantile(~ (rents_mean / income), national_data1, quantiles = .99, na.rm = TRUE)[1][[1]][1]
+    upper2 <- svyquantile(~ (rents_mean / income), national_data2, quantiles = .99, na.rm = TRUE)[1][[1]][1]
 
     # Check and print the number of valid data points
 
@@ -58,7 +58,7 @@ mtext("Distribution at Wave 1 (black) and Wave 4 (red)", side = 3, line = 1, out
 dev.off()
 
 national_data1 <- convey::convey_prep(national_data1)
-convey::svygini(~(rents_mean / income, national_data1), na.rm = TRUE)
+convey::svygini(~ (rents_mean / income), national_data1, na.rm = TRUE)
 
 national_data2 <- convey::convey_prep(national_data2)
-convey::svygini(~(rents_mean / income, national_data2), na.rm = TRUE)
+convey::svygini(~ (rents_mean / income), national_data2, na.rm = TRUE)
