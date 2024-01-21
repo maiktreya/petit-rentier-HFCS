c4a4adf9fc86b609b72a8154ad76d5978a5e481a
il    iciuv <miguel.garcia.duch@gmail.com>
2024-01-21 13:13:33 +0100

    last

--- src/main/explore-plot.R
+++ src/main/explore-plot.R
@@ -48,6 +48,7 @@ for (n in country_code) {
     df_cdf <- svycdf(as.formula(paste0("~", varname)), design = subset(national_data1, get(varname) < upper1 & get(varname) > 0))
     df_ecdf <- ecdf(subset(national_data2, get(varname) < upper2 & get(varname) > 0)$variables[, get(varname)])
     df_cdf[[1]] %>% plot(main = paste("Country:", n), lty = 1342, lwd = 1)
+    axis(1, at = seq(0, 1, by = 0.2))
     lines(df_ecdf, col = "#33FFFF", lty = 1, lwd = 2)
 }
 
--- tests.R
+++ tests.R
@@ -42,7 +42,8 @@ for (n in country_code) {
     df_cdf <- svycdf(as.formula(paste0("~", varname)), design = subset(national_data1, get(varname) > 0))
     df_ecdf <- ecdf(subset(national_data2, get(varname) > 0)$variables[, get(varname)])
     df_cdf[[1]] %>% plot(main = paste("Country:", n), lty = 1342, lwd = 1)
-    lines(df_ecdf, col = "#54ff5cda", lty = 1, lwd = 2)
+    axis(1, at = seq(0, 1, by = 0.2))
+    lines(df_ecdf, col = "#33ff70", lty = 1, lwd = 2)
 }
 
 # Add a general title and subtitle in the outer margin
