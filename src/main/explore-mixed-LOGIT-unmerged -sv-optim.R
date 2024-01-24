699cbf26541e0bace29945733e7448b963cf408d
il    iciuv <miguel.garcia.duch@gmail.com>
2024-01-24 22:08:55 +0100

    last

--- src/main/explore-mixed-LOGIT-unmerged -sv-optim.R
+++ src/main/explore-mixed-LOGIT-unmerged -sv-optim.R
@@ -20,11 +20,11 @@ for (i in 1:5) {
     start_time <- Sys.time()
     dataset_s <- dataset[implicate == i]
     model[[i]] <- glmer(
-        rentsbi_pens ~ factor(wave) + hsize + head_gendr + age + edu_ref +
+        rentsbi ~ factor(wave) + hsize + head_gendr + age + edu_ref +
             homeown + otherp +
             bonds + mutual + shares + managed + otherfin +
             haspvpens +
-            class_nomanager + soc_exp +
+            class_nomanager +
             (1 | sa0100) + (1 | sa0100:wave),
         family = binomial,
         data = dataset_s,
