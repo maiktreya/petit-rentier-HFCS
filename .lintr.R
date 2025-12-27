d58fc912a2ccbd487c6b6db9a5f809d5416a5399
maiktreya <garciaduchm@gmail.com>
2025-12-27 16:49:50 +0100

    deleted not needed R config files

--- .lintr.R
+++
@@ -1,9 +0,0 @@
-# Source - https://stackoverflow.com/a
-# Posted by Josiah Plett, modified by community. See post 'Timeline' for change history
-# Retrieved 2025-12-20, License - CC BY-SA 4.0
-
-linters <- linters_with_defaults(
-    line_length_linter = NULL,
-    commented_code_linter = NULL
-)
-encoding <- "UTF-8"
